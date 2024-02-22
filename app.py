f4247f7e1d930128bab36ed959faf03b2bf837ee
Thomas Kim <tkimm156@gmail.com>
2024-02-22 14:26:08 -0600

    get app to stop crashing

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
+
 
 debug = DebugToolbarExtension(app)
 
+responses = []
 
 @app.get('/')
 def start_page():
@@ -24,6 +25,7 @@ def start_page():
 
 @app.post('/begin')
 def begin_survey():
+    responses.clear()
 
     return redirect('/question/0')
 
@@ -33,5 +35,20 @@ def get_first_question():
 
     question = survey.questions[0]
 
+    return render_template("question.html",
+                           question=question)
+
+@app.post("/answer")
+def get_answer():
+    answer = request.form["answer"]
+    responses.append(answer)
+
+    return redirect("/question/1")
+
+@app.get('/question/1')
+def get_second_question():
+
+    question = survey.questions[1]
+    print("********************responses=",responses)
     return render_template("question.html",
                            question=question)
